2ade0f45bf073775b581604af9448c76de324a7c
CodeWithRohitRajput <rohitsinghrajput2k24@gmail.com>
2025-11-04 22:36:06 +0530

    add user detail at sidebar

--- client/(components)/sidebar/page.tsx
+++ client/(components)/sidebar/page.tsx
@@ -1,9 +1,11 @@
 'use client';
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Sidebar() {
   const [isCollapsed, setIsCollapsed] = useState(false);
+  const[name , setName] = useState<string>('')
+  const[email , setEmail] = useState<string>('')
 
 const menuItems = [
     { 
@@ -63,6 +65,19 @@ const menuItems = [
     },
   ];
 
+  useEffect(()=>{
+    const getUserDetail = async () => {
+      const res = await fetch('http://localhost:5000/auth/profile' , {
+        method : "GET",
+        credentials : "include"
+      })
+      const data = await res.json();
+      setName(data.name)
+      setEmail(data.email)
+    }
+   getUserDetail()
+  } , [])
+
   return (
     <div className={`bg-white shadow-lg border-r border-gray-200 h-screen sticky top-0 transition-all duration-300 ${isCollapsed ? 'w-20' : 'w-64'}`}>
       {/* Header */}
@@ -72,12 +87,12 @@ const menuItems = [
      <div className={` left-0 right-0 p-4 border-t border-gray-100 ${isCollapsed ? 'text-center' : ''}`}>
         <div className="flex items-center space-x-3">
           <div className="w-10 h-10 bg-gradient-to-br from-blue-600 to-purple-600 rounded-full flex items-center justify-center text-white font-semibold">
-            U
+            {name[0].toUpperCase()}
           </div>
           {!isCollapsed && (
             <div className="flex-1 min-w-0">
-              <p className="text-sm font-medium text-gray-900 truncate">User Name</p>
-              <p className="text-xs text-gray-500 truncate">user@invoico.com</p>
+              <p className="text-sm font-medium text-gray-900 truncate">{name || "User"}</p>
+              <p className="text-xs text-gray-500 truncate">{email || "User Email"}</p>
             </div>
           )}
         </div>
